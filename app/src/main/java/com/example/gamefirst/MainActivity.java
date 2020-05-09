a746e1a4e54d6ed188fb5e80d8e1419ef38d6791
Sharygin-Aleksei <mcmorattichannel@gmail.com>
2020-05-08 22:14:04 -0700

    added BarField layout

--- app/src/main/java/com/example/gamefirst/MainActivity.java
+++ app/src/main/java/com/example/gamefirst/MainActivity.java
@@ -3,37 +3,45 @@ package com.example.gamefirst;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ConstraintLayout myLayout = null;
+    ConstraintLayout gameFieldLayout = null;
+    ConstraintLayout barFieldLayout = null;
+    int gameFieldX;
+    int gameFieldY;
     int holePictureSideSize = 125;
+    PlayField playField = null;
+    Player player = null;
+    TextView dig = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final PlayField playField = new PlayField(10, 15);
+        gameFieldLayout = (ConstraintLayout)findViewById(R.id.gameField);
+        gameFieldLayout.setBackgroundResource(R.drawable.grass_background);
+        gameFieldX = gameFieldLayout.getLayoutParams().width;
+        gameFieldY = gameFieldLayout.getLayoutParams().height;
 
-        myLayout = (ConstraintLayout)findViewById(R.id.fon);
-        myLayout.setOnTouchListener(new View.OnTouchListener() {
+        barFieldLayout = (ConstraintLayout)findViewById(R.id.barField);
+
+        playField = new PlayField(gameFieldX/holePictureSideSize, gameFieldY/holePictureSideSize);
+        player = new Player();
+
+        dig = (TextView) findViewById(R.id.digCounter);
+        dig.setText(String.valueOf(player.dig));
+
+        gameFieldLayout.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
@@ -41,27 +49,32 @@ public class MainActivity extends AppCompatActivity {
                 float yTouch = event.getY();
 
                 if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    if(xTouch <= playField.fieldSizeX * holePictureSideSize && yTouch <= playField.fieldSizeY * holePictureSideSize ){
+                    if(xTouch <= playField.fieldSizeX * holePictureSideSize && yTouch <= playField.fieldSizeY * holePictureSideSize && player.dig > 0 ){
                         if(playField.field[(int) Math.floor(xTouch / holePictureSideSize)][(int) Math.floor(yTouch / holePictureSideSize)] == 0){
-
+                            player.dig--;
                             playField.field[(int) Math.floor(xTouch / holePictureSideSize)][(int) Math.floor(yTouch / holePictureSideSize)] = 1;
                             ImageView hole = new ImageView(MainActivity.this);
                             hole.setImageResource(R.drawable.holee);
                             hole.setX((float) (Math.floor(xTouch / holePictureSideSize)) * holePictureSideSize);
                             hole.setY((float) (Math.floor(yTouch / holePictureSideSize)) * holePictureSideSize);
-                            myLayout.addView(hole);
+                            gameFieldLayout.addView(hole);
                             hole.getLayoutParams().height = holePictureSideSize;
                             hole.getLayoutParams().width = holePictureSideSize;
                         }else{
                             Toast.makeText(getApplicationContext(), "ПШОЛ ВОН", Toast.LENGTH_SHORT).show();
                         }
                     }
+                    dig.setText(String.valueOf(player.dig));
                 }
                 return true;
             }
         });
-
-
+    }
+    public void startNewGame(View v){
+        gameFieldLayout.removeAllViews();
+        playField = new PlayField(gameFieldX/holePictureSideSize, gameFieldY/holePictureSideSize);
+        gameFieldLayout.setBackgroundResource(R.drawable.grass_background);
+        player = new Player();
     }
 
 
---
+++ app/src/main/java/com/example/gamefirst/Player.java
@@ -0,0 +1,5 @@
+package com.example.gamefirst;
+
+public class Player {
+    int dig = 10;
+}
